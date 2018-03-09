7e4000ba5ee64493fc20d23824093de5da4c18a1
Julie CARUSO <jcaruso@adneom.com>
2018-03-09 17:11:15 +0100

    Add project model

--- api/src/main/java/com/kapouter/api/model/response/ProjectsResponse.kt
+++ api/src/main/java/com/kapouter/api/model/response/ProjectsResponse.kt
@@ -1,6 +1,6 @@
 package com.kapouter.api.model.response
 
-import com.kapouter.api.model.Pattern
+import com.kapouter.api.model.Project
 
-data class ProjectsResponse(val projects: List<Pattern>) {
+data class ProjectsResponse(val projects: List<Project>) {
 }
